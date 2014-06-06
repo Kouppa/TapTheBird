1ee23cd7dd3488489554fb81a3c1d9fac1977f12
OneMCGuy <koutaminers@me.com>
2014-06-06 19:41:23 +0900

    Initial Commit

---
+++ TapTheBird/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TapTheBird
+//
+//  Created by Administrator on 6/6/14.
+//  Copyright (c) 2014 OneMinecraftGuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TapTheBirdTests/TapTheBirdTests.m
@@ -0,0 +1,34 @@
+//
+//  TapTheBirdTests.m
+//  TapTheBirdTests
+//
+//  Created by Administrator on 6/6/14.
+//  Copyright (c) 2014 OneMinecraftGuy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TapTheBirdTests : XCTestCase
+
+@end
+
+@implementation TapTheBirdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
