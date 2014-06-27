4e2d46e09ecd32b3e609bfb2548965551de0fd67
Kouppa <koutaminers@me.com>
2014-06-27 19:04:18 +0900

    Added the rest of the required files.

---
+++ TapTheBird/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  TapTheBird
+//
+//  Created by Administrator on 6/27/14.
+//  Copyright (c) 2014 OneMinecraftGuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ TapTheBird/WhatViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhatViewController.h
+//  TapTheBird
+//
+//  Created by Administrator on 6/27/14.
+//  Copyright (c) 2014 OneMinecraftGuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhatViewController : UIViewController
+
+@end
